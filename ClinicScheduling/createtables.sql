affae9450512f604f1fb80f171363025d5b07187
jmgooden94 <jmgooden94@gmail.com>
2017-01-09 20:58:17 -0600

    updated createtables.sql script

--- ClinicScheduling/createtables.sql
+++ ClinicScheduling/createtables.sql
@@ -121,6 +121,7 @@ CREATE TABLE IF NOT EXISTS `clinic`.`Recurrence` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
+CREATE TABLE IF NOT EXISTS `clinic`.`user`(`id` INTEGER(11) NOT NULL AUTO_INCREMENT, `username` varchar(30) NOT NULL UNIQUE, `role` varchar(30) NOT NULL, PRIMARY KEY(`id`));
 
 CREATE USER IF NOT EXISTS `clinic_admin` IDENTIFIED BY `defaultAdminPassword`;
 GRANT ALL ON clinic.* TO `clinic_admin` WITH GRANT OPTION;
